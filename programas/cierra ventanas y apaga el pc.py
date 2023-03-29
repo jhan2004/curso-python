fe2c88425fdbf86f1e03ce862f6c2c6eb6b4d47d
jhan2004 <jhancarlossaavedra2019@gmail.com>
2023-03-29 10:49:54 -0500

    commit

--- programas/cierra ventanas y apaga el pc.py
+++ programas/cierra ventanas y apaga el pc.py
@@ -3,5 +3,12 @@ import os
 # Cierra todas las ventanas abiertas
 os.system('taskkill /f /im explorer.exe')
 
-# Apaga el PC
-os.system('shutdown /s /t 0')
+# Pregunta al usuario si desea apagar el PC
+opcion = input("¿Desea apagar el PC? (Sí/No)")
+
+if opcion.lower() == "sí":
+    # Apaga el PC
+    os.system('shutdown /s /t 0')
+else:
+    print("El PC no se apagará.")
+    
